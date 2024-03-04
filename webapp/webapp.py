df341a5764d98994f03c114091326338620d54e3
Samuel Xavier <samuelxpp@gmail.com>
2024-03-04 20:18:57 -0300

    Developing webapp portion of the project

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -4,6 +4,7 @@ from fastapi.templating import Jinja2Templates
 import pandas as pd
 
 app = FastAPI()
+
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/table", response_class=HTMLResponse)
